cd737178cc5d7bc593a64a2b99a4eb322fb021a6
phucbm <hiphucbui@gmail.com>
2023-06-22 11:07:52 +0700

    feat: vertical scrolling inside

--- src/_index.js
+++ src/_index.js
@@ -19,12 +19,35 @@ class EasyHorizontalScrolling{
 
     // sync vertical scroll with horizontal scroll
     // ref: https://alvarotrigo.com/blog/scroll-horizontally-with-mouse-wheel-vanilla-java/
-    syncScroller(){
-        this.element.addEventListener("wheel", event => {
-            //evt.preventDefault(); // no prevent default to keep native horizontal scroll
+    syncScroller(element = this.element){
+        // watch wheel event on wrapper element
+        addWrapperEvent();
+
+        function wheelHandler(event){
+            //event.preventDefault(); // no prevent default to keep native horizontal scroll
             const scroll = event.deltaY;
 
-            this.element.scrollLeft += scroll;
+            element.scrollLeft += scroll;
+        }
+
+        function addWrapperEvent(){
+            element.addEventListener("wheel", wheelHandler);
+        }
+
+        function removeWrapperEvent(){
+            element.removeEventListener("wheel", wheelHandler);
+        }
+
+        /**
+         * For vertical scrolling inside
+         */
+        const verticalScroller = this.element.querySelectorAll('[data-ehs-vertical-scroll]');
+        verticalScroller.forEach(item => {
+            // enter -> remove wrapper event
+            item.addEventListener('mouseenter', removeWrapperEvent);
+
+            // leave -> re-assign wrapper event
+            item.addEventListener('mouseleave', addWrapperEvent);
         });
     }
 }
